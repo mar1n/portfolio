5f0e13e4b4f0bb44daaef22d5cf7ec702a8728c5
mar1n <30738216+mar1n@users.noreply.github.com>
2019-12-28 13:32:04 +0000

    add links in footer.

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './Footer.css';
-
+//import { Link } from 'react-router-dom';
 const Footer = () => {
     return <div className='footer'>
         <div className='item'>
             <div className='linkedin'>
-                
+                <a href={`https://www.linkedin.com/in/szymon-dawidowicz`}> </a>
             </div>
         </div>
         <div className='item'>
             <div className='github'>
-                
+                <a href={`https://github.com/mar1n/`}> </a>
             </div>
         </div>
         <div className='item'>
             <div className='email'>
-                
+                <a href={`mailto:szym0nd4widowicz@gmail.com`}> </a>
             </div>
         </div>
     </div>
