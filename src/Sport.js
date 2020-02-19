164bfdfca9dca28c45c8b5cab763499234a5a189
mar1n <30738216+mar1n@users.noreply.github.com>
2020-02-19 23:45:40 +0000

    remove comment

--- src/Sport.js
+++ src/Sport.js
@@ -8,16 +8,9 @@ export default function Sport(props) {
             <h2>{props.name}</h2>
             <div className='place'>
                 <div className="box">
-                    {/* <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span> */}
                     <img src={props.picture} alt={`${props.name} + logo`} />
                 </div>
             </div>
-            {/* <div className='overlay'>
-                <h1>{props.name}</h1>
-            </div> */}
         </Link>
     );
 }
