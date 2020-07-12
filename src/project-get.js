5c62b3008c9e2eb955ce380897ba5d72f65241ba
mar1n <30738216+mar1n@users.noreply.github.com>
2020-07-13 00:24:54 +0100

    add new project, change in description of project

--- src/project-get.js
+++ src/project-get.js
@@ -1,11 +1,11 @@
 import stopwatch from './img/stopwatch.png';
-import team from './img/team.png';
+import sport from './img/sport.png';
 import weather from './img/weather.png';
 
 export default function getProject() {
     return [
-      {id: 'stopwatch', name: 'Stopwatch', logo: stopwatch, address: 'https://stopwatch.cykcykacz.now.sh/', details:'A stopwatch is a handheld timepiece designed to measure the amount of time that elapses between its activation and deactivation. A large digital version of a stopwatch designed for viewing at a distance, as in a sports stadium, is called a stopclock. In manual timing, the clock is started and stopped by a person pressing a button. In fully automatic time, both starting and stopping are triggered automatically, by sensors.'},
-      {id: 'team', name: 'Team', logo: team, address: 'https://state-one-inky.now.sh/', details:'As defined by Professor Leigh Thompson of the Kellogg School of Management, "[a] team is a group of people who are interdependent with respect to information, resources, and skills and who seek to combine their efforts to achieve a common goal".'},
-      {id: 'weather', name: 'Weather', logo: weather, address: 'https://weatherengland.cykcykacz.now.sh/', details: 'Weather forecasting is the application of science and technology to predict the conditions of the atmosphere for a given location and time. People have attempted to predict the weather informally for millennia and formally since the 19th century. Weather forecasts are made by collecting quantitative data about the current state of the atmosphere at a given place and using meteorology to project how the atmosphere will change.'},
+      {id: 'stopwatch', name: 'Stopwatch', logo: stopwatch, address: 'https://stopwatch.cykcykacz.now.sh/', details:'This is simulation of stopwatch.'},
+      {id: 'sport', name: 'Sport', logo: sport, address: 'http://ec2-35-178-211-35.eu-west-2.compute.amazonaws.com:3001/', details:'This app is the list of sport rules. We can find information about sport and video with rule.'},
+      {id: 'weather', name: 'Weather', logo: weather, address: 'https://weatherengland.cykcykacz.now.sh/', details: 'The application show weather for chosen England city.'},
     ];
 }
